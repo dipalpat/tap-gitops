a3df5902460f6f8e7c7b02e278bbdc136e53d388
Dipal Patel <dipal.patel@gmail.com>
2023-05-11 11:45:29 -0400

    added build-green configuration

--- create-aws-secets.sh
+++ create-aws-secets.sh
@@ -9,14 +9,19 @@ EOF
 )"
 
 aws secretsmanager create-secret \
-   --name dev/${EKS_CLUSTER_NAME}/tanzu-sync/sync-git-ssh \
+   --name dev/${EKS_CLUSTER_NAME}/tanzu-sync/install-registry-dockerconfig \
    --secret-string "$(cat <<EOF
 {
- "ssh-privatekey": "$(cat ~/.ssh/id_ed25519 | awk '{printf "%s\\n", $0}')",
- "ssh-knownhosts": "$(ssh-keyscan github.com | awk '{printf "%s\\n", $0}')"
-}    
-EOF  
+ "auths": {
+   "${INSTALL_REGISTRY_HOSTNAME}": {
+     "username": "${INSTALL_REGISTRY_USERNAME}",
+     "password": "$(cat /Users/pdipal/Downloads/pa-dpatel-689de39b13dd.json)"
+   }
+ }
+}
+EOF
 )"
+
 
 aws secretsmanager create-secret \
    --name dev/${EKS_CLUSTER_NAME}/tap/sensitive-values.yaml \
